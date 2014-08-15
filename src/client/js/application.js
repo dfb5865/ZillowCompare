a9bbe1f30be31a0f6d01dd9537b62068080cdc20
Dave Banan <davowns@gmail.com>
2014-08-15 00:18:43 -0700

    First Mock up of UI
    Includes UI dependencies

---
+++ src/client/js/application.js
@@ -0,0 +1,26 @@
+$( "#searchButton" ).click(function() {
+  $( "#error" ).remove();
+  var address = $( "#searchText" ).val();
+  if(address === ""){
+	$( "#searchGroup" ).append( '<div id="error"><br/><div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> Please enter an address </div></div>');
+  } else{
+	var url = "/search/" + address;
+	window.location = url;
+  }  
+});
+
+$(function()
+{
+  $('input').each(function() 
+  {
+     $(this).val($(this).attr('holder'));
+  });
+  
+  $('input').focus(function()
+  {
+    if($(this).attr('holder')==$(this).val())
+    {
+      $(this).val('');
+    }
+  });
+});
